0ad26436969048db37c47ee736d47f73209ed06b
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2015-08-08 11:56:34 -0500

    database initialization

---
+++ models/models.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var path = require('path');
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBD SQLite
+var sequelize = new Sequelize(null, null, null, {
+    dialect: 'sqlite',
+    storage: 'quiz.sqlite'
+});
+
+// Importar la definicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz;
+
+// sequelize.sync() crea e inicializa tabla de preguntas en DB
+sequelize.sync().then(function () {
+    // then(..) ejecuta el manejador una vez creada la tabla
+    Quiz.count().then(function (count) {
+        if (count === 0) {
+            Quiz.create({
+                pregunta: 'Capital de Italia',
+                respuesta: 'Roma'
+            }).then(function () {
+                console.log('Base de datos inicializada');
+            });
+        }
+    })
+});
