ffc682082845aeb2b1de146ae08c6c6d1278e7e6
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2015-08-08 15:35:40 -0500

    heroku database adapter

--- models/models.js
+++ models/models.js
@@ -2,13 +2,30 @@
 
 var path = require('path');
 
+// Postgres: postgres://user:passwd@host:port/database
+// SQLite: sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DBname = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
 // Usar BBD SQLite
 var sequelize = new Sequelize(null, null, null, {
-    dialect: 'sqlite',
-    storage: 'quiz.sqlite'
+    dialect: protocol,
+    protocol: protocol,
+    port: port,
+    host: host,
+    storage: storage,
+    omitNull: true
 });
 
 // Importar la definicion de la tabla Quiz en quiz.js
